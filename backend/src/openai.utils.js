c116c72dffa5b6564b23307d02fa62aa3131f51c
jaswant57 <j.rajput@texau.app>
2025-03-15 01:25:42 +0530

    added openai utilities functions

---
+++ backend/src/openai.utils.js
@@ -0,0 +1,35 @@
+import OpenAI from "openai";
+import dotenv from "dotenv";
+dotenv.config();
+const openai = new OpenAI();
+
+export const appendMessageInThread = async (threadId, userMessage) => {
+  const message = await openai.beta.threads.messages.create(threadId, {
+    role: "user",
+    content: userMessage,
+  });
+  return message;
+};
+
+export const createThread = async () => {
+  const thread = await openai.beta.threads.create();
+  console.log(thread);
+  return thread;
+};
+
+export const createRun = async (threadId, assistantId) => {
+  const run = await openai.beta.threads.runs.createAndPoll(threadId, {
+    assistant_id: assistantId,
+  });
+  return run;
+};
+
+export const listMesasgesInThread = async (threadId) => {
+  const messages = await openai.beta.threads.messages.list(threadId);
+  return messages.data;
+};
+
+export const delThread = async (threadId) => {
+  const response = await openai.beta.threads.del(threadId);
+  return response;
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,13 @@
 import express from "express";
-
+import dotenv from "dotenv";
+dotenv.config();
+import {
+  appendMessageInThread,
+  createThread,
+  listMesasgesInThread,
+  createRun,
+  delThread,
+} from "./openai.utils.js";
 const app = express();
 
 app.use(express.json());
