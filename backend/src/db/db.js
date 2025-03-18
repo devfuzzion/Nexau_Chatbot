3b937df79ddbe821ed439a08cdc802fac8135a9d
jaswant57 <j.rajput@texau.app>
2025-03-18 23:42:56 +0530

    feat: changed the db provider

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,9 +1,21 @@
-import postgres from "postgres";
+import fs from "fs";
+import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-const connectionString = process.env.DATABASE_URL;
-const sql = postgres(connectionString, {
-  ssl: "require",
-});
-export default sql;
+const config = {
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync("./ca.pem").toString(),
+  },
+};
+
+const client = new pg.Pool(config); // Use Pool for better connection handling
+
+
+export default client;
--- backend/src/db/threads.queries.js
+++ backend/src/db/threads.queries.js
@@ -1,17 +1,24 @@
 import sql from "./db.js";
-import { listMesasgesInThread } from "./../openai.utils.js";
 
 export const getAllThreads = async () => {
-  const threads = await sql`SELECT * FROM threads`;
-  return threads;
+  try {
+    const { rows } = await sql.query("SELECT * FROM threads");
+    return rows;
+  } catch (err) {
+    console.error("Error fetching threads:", err);
+    throw err;
+  }
 };
 
 export const createThreadInDb = async (threadId) => {
   try {
-    const [thread] =
-      await sql`INSERT INTO threads ("threadId") VALUES (${threadId})`;
-    return thread;
+    const { rows } = await sql.query(
+      'INSERT INTO threads ("threadid") VALUES ($1) RETURNING *',
+      [threadId],
+    );
+    return rows[0];
   } catch (err) {
-    console.log(err);
+    console.error("Error inserting thread:", err);
+    throw err;
   }
 };
