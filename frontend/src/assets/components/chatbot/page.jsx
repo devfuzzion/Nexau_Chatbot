12081660a617989cabc95af001501886b9821cca
jaswant57 <j.rajput@texau.app>
2025-04-05 19:37:22 +0530

    fix: updating the backend url for feedback

--- frontend/src/assets/components/chatbot/page.jsx
+++ frontend/src/assets/components/chatbot/page.jsx
@@ -24,8 +24,8 @@ const Chatbot = () => {
   const createThread = async () => {
     try {
       const response = await fetch(
-        // "https://ejitukppt8.execute-api.eu-west-3.amazonaws.com/dev/create-thread",
-        "http://localhost:3000/create-thread",
+        "https://ejitukppt8.execute-api.eu-west-3.amazonaws.com/dev/create-thread",
+        // "http://localhost:3000/create-thread",
         {
           method: "POST",
           headers: {
